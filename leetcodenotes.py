7b0519a60f0d7453e49c487dd833a68e75ca460f
Tysuiku <timothydong41@gmail.com>
2024-09-02 06:38:30 -0400

    code

--- leetcodenotes.py
+++ leetcodenotes.py
@@ -1,2 +1,39 @@
 #Leetcode Notes
 
+#217. Contains Duplicate
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        numset = set()
+
+        for n in nums:
+            if n in numset:
+                return True
+            else:
+                numset.add(n)
+        
+        return False
+
+#242. Valid Anagram
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        map1 = {}
+        map2 = {}
+
+        for c in s:
+            map1[c] = map1.get(c,0)+1
+
+        for c in t:
+            map2[c] = map2.get(c,0)+1
+
+        return map1 == map2
+    
