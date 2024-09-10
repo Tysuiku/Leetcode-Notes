9cbd75f899c478f14e24a741a8c56789152cbf1b
Tysuiku <timothydong41@gmail.com>
2024-09-10 14:52:18 -0400

    code

--- leetcodenotes.py
+++ leetcodenotes.py
@@ -122,3 +122,22 @@ class Solution(object):
             #res = [24,12,8,6]
 
         return res
+    
+#128. Longest Consecutive Sequence
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if n - 1 not in numSet:
+                length = 0
+                while n + length in numSet:
+                    length += 1
+                longest = max(longest, length)
+
+        return longest
